1b4fd4a935ee10395216b4c2810d9e4ac1929c80
Tihomir Selak <tknox.dr@gmail.com>
2021-09-17 21:01:16 +0200

    RCC-01: Add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,46 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: auto 1fr;
+  grid-template-areas: "header header" "sidebar content";
+`;
+
+const Header = styled.header`
+  grid-area: header;
+  padding: 1em;
+  background-color: #666;
+  color: white;
+  text-align: center;
+  font-weight: bold;
+`;
+
+const Sidebar = styled.nav`
+  grid-area: sidebar;
+  background-color: #ccc;
+  padding: 1em;
+`;
+
+const Content = styled.main`
+  grid-area: content;
+  padding: 1em;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="header">
-        The Recipe Collection
-      </header>
+    <AppContainer>
+      <Header>The Recipe Collection</Header>
 
-      <nav className="sidebar">
+      <Sidebar>
         <p>Search recipes</p>
         <p>Bookmarked recipes</p>
-      </nav>
+      </Sidebar>
 
-      <main className="content">
-        [content]
-      </main>
-    </div>
+      <Content>[content]</Content>
+    </AppContainer>
   );
 }
 
