d42e2ff2ca5de0bb612876dbf6e19fe7a3a3a829
Tihomir Selak <tknox.dr@gmail.com>
2021-09-17 23:10:21 +0200

    RCC-01: Add reselect types

--- src/themes/selectors/colors.ts
+++ src/themes/selectors/colors.ts
@@ -4,16 +4,16 @@ import { ThemeProps } from "styled-components";
 
 const themeColors = (state: ThemeProps<any>) => get(state, ["theme", "colors"], {});
 
-export const focusColor = createSelector(themeColors, colors => get(colors, "focus"));
+export const focusColor = createSelector(themeColors, (colors: object) => get(colors, "focus"));
 
 export const primaryColor = createSelector(
   themeColors,
-  colors => get(colors, "primary")
+  (colors: object) => get(colors, "primary")
 );
 
 export const secondaryColor = createSelector(
   themeColors,
-  colors => get(colors, "secondary")
+  (colors: object) => get(colors, "secondary")
 );
 
 export const defaultTextColor = primaryColor;
